9dab481883e0bbda466b093cce2352b1b0574658
Cesar <134793116+cchaaya@users.noreply.github.com>
2023-08-13 23:56:52 +0300

    Update adocare.py

--- adocare.py
+++ adocare.py
@@ -319,8 +319,6 @@ conv_qa = ConversationalRetrievalChain.from_llm(
 #         print("\033[1mAdocare reply:\033[0m", result['answer'])
 
 
-
-
 # def main():
 #     st.title("Adocare Chatbot")
 
@@ -330,18 +328,17 @@ conv_qa = ConversationalRetrievalChain.from_llm(
 #         # Perform conversational question-answering using the model
 #         result = conv_qa({"question": user_question})
 
-#         # Display the answer
-#         st.text("Adocare reply: " + result['answer'])
+#         # Display the question and answer in a fixed-size text area
+#         st.text("User question: " + user_question)
+#         st.text_area("Adocare reply:", result['answer'], height=200)
 
 # if __name__ == "__main__":
 #     main()
 
-import streamlit as st
-
-# Import necessary packages and set up your model here
-
 def main():
     st.title("Adocare Chatbot")
+
+    conversation_history = []  # Initialize an empty list to store conversation history
 
     user_question = st.text_input("User question:")
 
@@ -349,11 +346,12 @@ def main():
         # Perform conversational question-answering using the model
         result = conv_qa({"question": user_question})
 
-        # Display the question and answer in a fixed-size text area
-        st.text("User question: " + user_question)
-        st.text_area("Adocare reply:", result['answer'], height=200)
+        # Add the user question and Adocare's reply to the conversation history
+        conversation_history.append(("User: " + user_question, "Adocare: " + result['answer']))
+
+        # Display the conversation history in a text area
+        st.text_area("Conversation History:", "\n".join(["\n".join(item) for item in conversation_history]), height=400)
 
 if __name__ == "__main__":
     main()
-
 
