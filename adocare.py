ea23c4d92e5a0fa95d7d50d0c320bae5f48989a7
Cesar <134793116+cchaaya@users.noreply.github.com>
2023-08-14 23:21:42 +0300

    Update adocare.py

--- adocare.py
+++ adocare.py
@@ -210,15 +210,21 @@ conv_qa = ConversationalRetrievalChain.from_llm(
 # if __name__ == "__main__":
 #     main()
 
+import streamlit as st
+
 def main():
     conversation = []  # Initialize an empty list to store the conversation history
 
     # Add the image next to the title
-    col1, col2 = st.columns([1, 3])  # Divide the screen into columns
+    col1, col2, col3, col4 = st.columns([0.25, 0.25, 0.25, 0.25])  # Divide the screen into columns
     with col1:
-        st.image("Group_pic.png", use_column_width=True)  # Display the image
-    with col2:
         st.title("Adocare Chatbot")  # Display the title
+    with col2:
+        st.write("")  # Empty column for spacing
+    with col3:
+        st.write("")  # Empty column for spacing
+    with col4:
+        st.image("Group_pic.png", use_column_width=True)  # Display the image
 
     st.subheader("Feel free to ask any questions you have!")
     st.markdown("This is a demo Chatbot designed to respond to adolescents' inquiries related to sexual and reproductive health."
@@ -244,4 +250,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
