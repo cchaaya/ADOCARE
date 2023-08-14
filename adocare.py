91db6637700c15942c83be03897a5fe33ac30376
Cesar <134793116+cchaaya@users.noreply.github.com>
2023-08-14 18:21:11 +0300

    Update adocare.py

--- adocare.py
+++ adocare.py
@@ -32,8 +32,8 @@ import streamlit as st
 load_dotenv()
 
 # Set the OpenAI API key for authentication
-os.environ['OPENAI_API_KEY'] = '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'
-
+# os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 ## PDFs
 
 # The code initializes PDF loaders and loads multiple PDF documents into a list
